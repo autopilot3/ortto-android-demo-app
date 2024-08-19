2ee96349baed62af0126a98c11b28999b22f3cdc
Mitchell Flindell <mitch.flindell@gmail.com>
2024-08-19 14:05:13 +1000

    adds user identity

--- app/src/main/java/com/ortto/demoapp/DemoApplication.java
+++ app/src/main/java/com/ortto/demoapp/DemoApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.ortto.messaging.Ortto;
 import com.ortto.messaging.OrttoConfig;
+import com.ortto.messaging.identity.UserID;
 
 public class DemoApplication extends Application {
 
@@ -11,14 +12,24 @@ public class DemoApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        // Initialize Ortto
         OrttoConfig config = new OrttoConfig(
-                getString(R.string.ortto_datasource_id),
-                getString(R.string.ortto_api_endpoint),
-                false,
-                true
+            getString(R.string.ortto_datasource_id),
+            getString(R.string.ortto_api_endpoint),
+            false,
+            true
         );
         Ortto.instance().init(config, this);
 
+
+        // Identify the user
+        UserID userID = UserID.make()
+            .setName("John", "Doe")
+            .setEmail("john@example.com")
+            .setExternalId("abc123");
+        Ortto.instance().identify(userID);
+
+        // Dispatch push permission request
         Ortto.instance().dispatchPushRequest();
     }
 }
